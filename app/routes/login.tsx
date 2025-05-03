92a4ce4ca48639d518bde8f40c88710d475c683e
misilelab <misileminecord@gmail.com>
2025-05-03 16:10:19 +0900

    refactor(login): inherit everything
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -6,21 +6,21 @@ export default function Login() {
     <Screen>
       <Header />
       <CenterFlex height="calc(100dvh - 168px)">
-        <CenterFlex style={{maxWidth: "400px"}} gap={40}>
+        <CenterFlex maxwidth="400px" gap={40}>
           <CenterFlex height="fit-content" gap={14}>
             <Title>로그인</Title>
             <Description>로그인하여 Scholub 커뮤니티를 이용하세요!</Description>
           </CenterFlex>
           <Container gap={30}>
-            <Input type="text" placeholder="이메일" />
-            <Input type="password" placeholder="비밀번호" />
-            <RowBetweenFlex>
-              <RowFlex>
+            <TextInput type="text" placeholder="이메일" />
+            <TextInput type="password" placeholder="비밀번호" />
+            <BetweenFlex direction="row">
+              <CenterFlex direction="row" gap={4} width={"fit-content"}>
                 <CheckBox type="checkbox" />
                 <Text>로그인 유지</Text>
-              </RowFlex>
+              </CenterFlex>
               <Text>계정 찾기</Text>
-            </RowBetweenFlex>
+            </BetweenFlex>
           </Container>
           <Submit type="submit" value="로그인" />
         </CenterFlex>
@@ -29,36 +29,31 @@ export default function Login() {
   );
 }
 
-const Flex = styled.div<{gap?: number, height?: string, width?: string}>`
+const Flex = styled.div<{
+  direction?: "row" | "column",
+  gap?: number,
+  height?: string,
+  width?: string,
+  maxwidth?: string
+}>`
   ${props => props.gap ? `gap: ${props.gap}px;` : ""}
   ${props => props.height ? `height: ${props.height};` : ""}
   ${props => props.width ? `width: ${props.width};` : ""}
+  ${props => props.maxwidth ? `max-width: ${props.maxwidth};` : ""}
   display: flex;
+  flex-direction: ${props => props.direction || "column"};
+  margin: 0;
+  padding: 0;
 `
 
-const ColFlex = styled(Flex)`
-  flex-direction: column;
-`
-
-const RowFlex = styled(Flex)`
-  flex-direction: row;
-`
-
-const RowCenterFlex = styled(RowFlex)`
-  justify-content: center;
-  align-items: center;
-`;
-
-const Screen = styled(ColFlex)`
+const Screen = styled(Flex)`
   justify-content: center;
   align-items: center;
   height: 100dvh;
 `
 
-const CenterFlex = styled(ColFlex)`
-  margin: 0;
-  padding: 0;
-  width: 100%;
+const CenterFlex = styled(Flex)`
+  width: ${props => props.width || "100%"};
   justify-content: center;
   align-items: center;
 `
@@ -67,22 +62,20 @@ const Container = styled(CenterFlex)`
   max-width: 400px;
 `
 
-const RowBetweenFlex = styled(Container)`
-  flex-direction: row;
+const BetweenFlex = styled(Container)`
   justify-content: space-between;
-  align-items: center;
-  width: 100%;
+  height: fit-content;
 `;
 
 const Text = styled.span`
-  text-color: #322F29;
   color: #322F29;
-  font-family: NanumSquareRound;
   font-size: 14px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
   letter-spacing: -0.14px;
+  text-align: center;
+  height: fit-content;
 `
 
 const Title = styled(Text)`
@@ -96,13 +89,6 @@ const Description = styled(Text)`
 `
 
 const Input = styled.input`
-  &::placeholder {
-  text-color: rgba(50, 47, 41, 0.4);
-  }
-  &:focus {
-    outline: none;
-    border-bottom: 1px solid #F7971D;
-  }
   border: none;
   min-width: 400px;
   display: flex;
@@ -114,15 +100,20 @@ const Input = styled.input`
   border-bottom: 1px solid #DDD;
 `;
 
-const CheckBox = styled.input`
-  border: none;
+const TextInput = styled(Input)`
+  &::placeholder {
+    text-color: rgba(50, 47, 41, 0.4);
+  }
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid #F7971D;
+  }
+`;
+
+const CheckBox = styled(Input)`
   max-width: 400px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  border-bottom: 1px solid #F7971D;
-  margin: 0;
+  min-width: fit-content;
+  height: fit-content;
   margin-right: 4px;
 `
 

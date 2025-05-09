3a30196f9e1b8aaa59f4cd0d165999a4cb77679f
박찬규 <126973231+pck-llight@users.noreply.github.com>
2025-05-09 10:42:31 +0900

    article component

---
+++ app/components/Article.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export interface ArticleProps {
+  title: string;
+  subTitle: string;
+  imgUrl: string;
+  category: string;
+}
+
+export default function Article(props: ArticleProps) {
+  return (
+    <ArticleContainer>
+      <ArticleImg/>
+      <ArticleTitle>{props.title}</ArticleTitle>
+      <ArticleSubTitle>{props.subTitle}</ArticleSubTitle>
+      <ArticleCategory>{props.category}</ArticleCategory>
+    </ArticleContainer>
+  )
+}
+
+const ArticleContainer = styled.div`
+  display: flex;
+  padding: 10px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
+`
+const ArticleImg = styled.img`
+  height: 159px;
+  align-self: stretch;
+  border-radius: 4px;
+  border: 1px solid #DDD;
+  background-color: #696969;
+`
+const ArticleTitle = styled.span`
+  align-self: stretch;
+  color: #322F29;
+  font-family: NanumSquareRound;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px; /* 133.333% */
+  letter-spacing: -0.36px;
+`
+const ArticleSubTitle = styled.span`
+  overflow: hidden;
+  color: #322F29;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-family: NanumSquareRound;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%; /* 16.8px */
+  letter-spacing: -0.28px;
+`
+const ArticleCategory = styled.span`
+  color: #F7971D;
+  text-align: center;
+  font-family: NanumSquareRound;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px; /* 133.333% */
+  letter-spacing: -0.12px;
+`

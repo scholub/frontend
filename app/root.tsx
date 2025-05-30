49ffa24d6bf4f926f2f020f6144e6ddd34d1b570
Chaeho Yu <chaeho1234@naver.com>
2025-05-30 09:21:27 +0900

    feat: implement ErrorBoundary component for error handling

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./index.css";
+import { ErrorBoundary } from "./components/ErrorBoundary"
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff" }
@@ -34,33 +35,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
-}
-
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "존재하지 않는 페이지 입니다."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
 }

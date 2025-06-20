c6fd69bc43bea019880c9568ceb2b89fb419aaac
misilelab <misileminecord@gmail.com>
2025-06-20 16:04:04 +0900

    feat: pwa
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), svgr()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    svgr(),
+    VitePWA({ registerType: "autoUpdate" })
+  ],
   optimizeDeps: {
     include: ["styled-components"],
   },
